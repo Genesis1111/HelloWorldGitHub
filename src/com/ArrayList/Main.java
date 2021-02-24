a8bc3f25f64066023ec6aa0fec189e62c6c96a80
Genesis1111 <51053786+Genesis1111@users.noreply.github.com>
2021-02-24 23:53:07 +0100

    Add was geht ab

--- src/com/ArrayList/Main.java
+++ src/com/ArrayList/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	// write your code here
 
         System.out.println("Hello");
-        System.out.println("next Line");
+        System.out.println("next Line was geht ab");
     }
 }
