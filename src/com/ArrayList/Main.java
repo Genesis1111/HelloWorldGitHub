c3c8f6b6fdb05e6c7fe34821ac8425aa57e0780c
Genesis <gen.esis1@web.de>
2021-02-25 23:34:15 +0100

    erste Änderung

--- src/com/ArrayList/Main.java
+++ src/com/ArrayList/Main.java
@@ -9,6 +9,7 @@ public class Main {
 <<<<<<< HEAD
         System.out.println("next Line");
         System.out.println("next next Line");
+        System.out.println("next next Line");
 =======
         System.out.println("next Line was geht ab");
 >>>>>>> a8bc3f25f64066023ec6aa0fec189e62c6c96a80
